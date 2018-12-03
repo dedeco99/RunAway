510110034ae7bc056af24a17b28988dcfc2ab069
mundoex <brunoserrao19@gmail.com>
2018-12-03 18:23:10 +0100

    +Improved Location +MapFragment refactor

--- app/src/main/java/com/runaway/runaway/MapFragment.java
+++ app/src/main/java/com/runaway/runaway/MapFragment.java
@@ -2,47 +2,32 @@ package com.runaway.runaway;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
-import android.net.ConnectivityManager;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Stack;
+import java.util.List;
 
-public class MapFragment extends Fragment implements AdapterView.OnItemSelectedListener, OnMapReadyCallback {
+public class MapFragment extends Fragment implements AdapterView.OnItemSelectedListener, OnMapReadyCallback{
     private static final String[] PERMISSIONS=new String[]{
             Manifest.permission.INTERNET,
             Manifest.permission.ACCESS_COARSE_LOCATION,
@@ -53,8 +38,9 @@ public class MapFragment extends Fragment implements AdapterView.OnItemSelectedL
     private static final float ZOOM=14.20f;
 
     private GoogleMap map;
+    private LocationManager locationManager;
+    private Location location;
 
-    private MarkerOptions myMarker;
     private Route selectedRoute;
     private HashMap<String,Route> routes;
 
@@ -64,117 +50,39 @@ public class MapFragment extends Fragment implements AdapterView.OnItemSelectedL
         View view=inflater.inflate(R.layout.fragment_map,container,false);
 
         //permissions check and request
-        if(getPermissionsMissing(PERMISSIONS).length>0){
-            askPermissions();
-            return reloadView(R.string.permissions_reload);
+        if(RequirementsUtils.isPermissionsMissing(getContext(),PERMISSIONS)){
+            RequirementsUtils.askPermissions(getActivity(), RequirementsUtils.getPermissionsMissing(getContext(),PERMISSIONS));
+            return RequirementsUtils.getFragmentReloadView(getActivity(), R.string.permissions_reload);
         }
 
-        //internet on check and request
-        ConnectivityManager cm=(ConnectivityManager)getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-        if(cm.getActiveNetworkInfo()==null){
-            checkInternet();
-            return reloadView(R.string.internet_reload);
+        //internet check and request
+        if(!RequirementsUtils.isInternetOn(getContext())){
+            RequirementsUtils.requestInternetDialog(getContext());
+            return RequirementsUtils.getFragmentReloadView(getActivity(),R.string.internet_reload);
         }
 
-        //gps on check and request
-        LocationManager locationManager=(LocationManager)getContext().getSystemService(Context.LOCATION_SERVICE);
-        if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
-            checkGPS();
-            return reloadView(R.string.gps_reload);
+        //gps check and request
+        if(!RequirementsUtils.isGPSOn(getContext())){
+            RequirementsUtils.requestGPSDialog(getContext());
+            return RequirementsUtils.getFragmentReloadView(getActivity(),R.string.gps_reload);
         }
+        //Routes and map Setup
+        fillRoutesMap();
+        initializeLocationManager();
+        initializeMap();
 
-           Spinner spinner=(Spinner)view.findViewById(R.id.maps_spinner);
-           fillRoutesMap();
-           initializeMap();
-
-           final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_spinner_item);
-           adapter.add(getString(R.string.spinner_routes));
-           adapter.addAll(routes.keySet());
-
-           adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-           spinner.setPrompt(getString(R.string.spinner_routes));
-           spinner.setAdapter(adapter);
-           spinner.setOnItemSelectedListener(this);
+        //Gui setup
+        Spinner spinner=view.findViewById(R.id.maps_spinner);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item);
+        adapter.add(getString(R.string.spinner_routes));
+        adapter.addAll(routes.keySet());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setPrompt(getString(R.string.spinner_routes));
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
         return view;
     }
-
-  //############################################PERMISSIONS AND SERVICES######################################################################
-
-    private String[] getPermissionsMissing(String... permissions){
-        Stack<String> neededPermissions=new Stack<String>();
-            for(int i=0;i<permissions.length;i++){
-                if(ContextCompat.checkSelfPermission(getContext(),permissions[i])!=PackageManager.PERMISSION_GRANTED){
-                    neededPermissions.push(permissions[i]);
-                    }
-                }
-        return neededPermissions.toArray(new String[0]);
-            }
-
-    private void askPermissions(){
-        String[] missingPermissions=getPermissionsMissing(PERMISSIONS);
-            if(missingPermissions.length!=0){
-                ActivityCompat.requestPermissions(getActivity(),missingPermissions,1);
-            }
-    }
-
-    /**
-     * If user does'nt have internet requests to connect
-     */
-    private void checkInternet(){
-
-                ConnectivityManager cm=(ConnectivityManager)getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-                if(cm.getActiveNetworkInfo()==null){
-                    AlertDialog.Builder builder = new AlertDialog.Builder(getContext(),R.style.LightDialogTheme);
-                    builder.setMessage(getString(R.string.connect_to_internet))
-                            .setCancelable(false)
-
-                            .setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int id) {
-                                    startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
-                                }
-                            })
-
-                            .setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int id){
-                                    getActivity().finish();
-                                }
-                            });
-
-                    AlertDialog alert = builder.create();
-                    alert.setTitle(getString(R.string.internet_connection_required));
-                    alert.show();
-            }
-        }
-
-    private void checkGPS(){
-              LocationManager locationManager=(LocationManager)getContext().getSystemService(Context.LOCATION_SERVICE);
-                if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
-                    AlertDialog.Builder builder = new AlertDialog.Builder(getContext(),R.style.LightDialogTheme);
-                    builder.setMessage(getString(R.string.turn_on_gps))
-                            .setCancelable(false)
-
-                            .setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int id) {
-                                    startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-                                }
-                            })
-
-                            .setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int id) {
-                                    getActivity().finish();
-                                }
-                            });
-
-                    AlertDialog alert = builder.create();
-                    alert.setTitle(getString(R.string.gps_service_required));
-                    alert.show();
-                }
-            }
 
 //############################################Initializations######################################################################
     private void initializeMap() {
@@ -184,24 +92,33 @@ public class MapFragment extends Fragment implements AdapterView.OnItemSelectedL
         }
     }
 
+    private void initializeLocationManager(){
+        //Better safe than broken
+        if(locationManager!=null &&
+           ContextCompat.checkSelfPermission(getContext(),Manifest.permission.ACCESS_COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED  &&
+           ContextCompat.checkSelfPermission(getContext(),Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED &&
+           ContextCompat.checkSelfPermission(getContext(),Manifest.permission.ACCESS_NETWORK_STATE)==PackageManager.PERMISSION_GRANTED){
+
+            locationManager=(LocationManager)getContext().getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map=googleMap;
-        updateMyMarker();
-        map.addMarker(myMarker);
-        map.moveCamera(CameraUpdateFactory.newLatLng(myMarker.getPosition()));
+        drawMyMarker();
     }
 
-    private void updateMyMarker(){
-        LocationManager locationManager=(LocationManager)getContext().getSystemService(Context.LOCATION_SERVICE);
-        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        LatLng gps = new LatLng(location.getLatitude(), location.getLongitude());
-        myMarker=new MarkerOptions().position(gps).title(getString(R.string.current_position)).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+    private void updateMyLocation() {
+        location=getCurrentLocation();
+        if(location==null){
+            Toast.makeText(getContext(), getString(R.string.no_location_found), Toast.LENGTH_LONG).show();
+        }
     }
 
     private void fillRoutesMap(){
-    routes=new HashMap<>();
-    //Faki get routes api
+        routes=new HashMap<>();
+        //Fake get routes api
         ArrayList<Route> apiRequest=new ArrayList<>();
         apiRequest.add(new Route("Praia do Credo",new LatLng(38.47946186,-368.9813447),new LatLng(38.47845399,-368.98658037)));
         apiRequest.add(new Route("Santa Claus House",new LatLng(66.5433403,25.8450086),new LatLng(66.543187,25.8438283)));
@@ -209,14 +126,26 @@ public class MapFragment extends Fragment implements AdapterView.OnItemSelectedL
         apiRequest.add(new Route("Kowalski analysis",new LatLng(-77.8979011,88.9252346),new LatLng(-71.985353, 137.287295)));
 
         apiRequest.add(new Route("Bairro da Formiga",new LatLng(38.00363641,-8.69360805),new LatLng(38.00356878, -8.69456291),new LatLng(38.00204704,-8.69464874)));
-        //actual code
+
+        //actual code cant use streams API 24 REQUIRED
         for(Route r: apiRequest){
             routes.put(r.getRouteName(),r);
         }
     }
 
-    private void setSelectedRoute(Route route){
+    public void setSelectedRoute(Route route){
         selectedRoute=route;
+    }
+
+    public void drawMyMarker(){
+        updateMyLocation();
+        if(location==null){
+            Toast.makeText(getContext(), getString(R.string.no_location_found), Toast.LENGTH_LONG).show();
+        }else{
+            LatLng gps = new LatLng(location.getLatitude(), location.getLongitude());
+            map.addMarker(new MarkerOptions().position(gps).title(getString(R.string.current_position)).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        }
+
     }
 
     /**
@@ -226,63 +155,48 @@ public class MapFragment extends Fragment implements AdapterView.OnItemSelectedL
      * Draws route
      * Moves camera to start marker
      */
-    public void drawRoute(){
+    public void drawSelectedRoute(){
         map.clear();
-        updateMyMarker();
-        map.addMarker(myMarker);
+        drawMyMarker();
         map.addMarker(selectedRoute.getStartMarker().title(getString(R.string.start_position)));
         map.addMarker(selectedRoute.getEndMarker().title(getString(R.string.end_position)));
         map.addPolyline(selectedRoute.getPolylineOptions());
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(selectedRoute.getStartLatLng(),ZOOM));
-
     }
 
+    private Location getCurrentLocation() {
+        Location bestLocation = null;
+        if (locationManager != null &&
+                ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_NETWORK_STATE) == PackageManager.PERMISSION_GRANTED) {
 
-    /**
-     * Selected route option
-     * Sets currentRoute to the selected one
-     * Draws route
-     * @param parent
-     * @param view
-     * @param position
-     * @param id
-     */
+            List<String> providers = locationManager.getProviders(true);
+
+            for (String provider : providers) {
+                Location l = locationManager.getLastKnownLocation(provider);
+
+                if (l == null) { continue; }
+
+                if (bestLocation == null
+                        || l.getAccuracy() < bestLocation.getAccuracy()) {
+                    bestLocation = l;
+                }
+            }
+        }
+        return bestLocation;
+    }
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         if (position != 0) {
             String text = parent.getItemAtPosition(position).toString();
             setSelectedRoute(routes.get(text));
             Toast.makeText(parent.getContext(), selectedRoute.getRouteString(), Toast.LENGTH_LONG).show();
-            drawRoute();
+            drawSelectedRoute();
         }
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) { }
-
-    private View reloadView(int id){
-        final LayoutInflater inflater = LayoutInflater.from(this.getActivity());
-        View reloadView=inflater.inflate(R.layout.reload, null);
-
-        TextView textView=reloadView.findViewById(R.id.infoText);
-        textView.setText(getString(id));
-
-        Button button=reloadView.findViewById(R.id.reloadButton);
-        button.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Fragment currentFragment=getActivity().getSupportFragmentManager().findFragmentById(R.id.fragment_container);
-                FragmentTransaction fragTrasaction=getActivity().getSupportFragmentManager().beginTransaction();
-                fragTrasaction.detach(currentFragment);
-                fragTrasaction.attach(currentFragment);
-                fragTrasaction.commit();
-            }
-        });
-
-        return reloadView;
-    }
-
-
-
 }
